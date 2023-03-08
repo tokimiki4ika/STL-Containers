c7c92aaa63eb6174bdb11b60ca6832df88c92e75
Nikita Yaroshenko <yaroshenkonikita4@gmail.com>
2023-03-08 14:51:15 +0300

    refactor

---
+++ src/containers.h
@@ -0,0 +1,11 @@
+#ifndef CPP2_S21_CONTAINERS_0_SRC_CONTAINERS_H_
+#define CPP2_S21_CONTAINERS_0_SRC_CONTAINERS_H_
+
+#include "./SortedContainer/AssociativeContainer/map.h"
+#include "./ContainerAdaptor/queue.h"
+#include "./ContainerAdaptor/stack.h"
+#include "./SequenceContainer/list.h"
+#include "./SequenceContainer/vector.h"
+#include "./SortedContainer/SetContainer/set.h"
+
+#endif //CPP2_S21_CONTAINERS_0_SRC_CONTAINERS_H_
---
+++ src/containersplus.h
@@ -0,0 +1,8 @@
+#ifndef CPP2_S21_CONTAINERS_0_SRC_CONTAINERSPLUS_H_
+#define CPP2_S21_CONTAINERS_0_SRC_CONTAINERSPLUS_H_
+
+#include "./StdContainer/array.h"
+#include "./SortedContainer/SetContainer/multiset.h"
+
+
+#endif //CPP2_S21_CONTAINERS_0_SRC_CONTAINERSPLUS_H_
